2465c88d03db44d97b99e4851e2a404dd5348d6d
Dusan Todorovic <dtodorovic40@gmail.com>
2022-09-15 18:19:53 +0200

    added size limit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'main_page.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Menu(),
+    return FlutterWebFrame(
+      builder: (context) {
+        return MaterialApp(
+          title: 'GDSC Demo Quiz',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: Menu(),
+        );
+      },
+      maximumSize: const Size(475.0, 812.0),
+      backgroundColor: Colors.white,
     );
   }
 }
