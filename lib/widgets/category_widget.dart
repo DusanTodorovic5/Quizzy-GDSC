9a981d4c9066f3a063d0d18926fe355ffab1420c
Dusan Todorovic <dtodorovic40@gmail.com>
2022-09-15 18:11:32 +0200

    Change greyscale filter to matrix,now works on web

--- lib/widgets/category_widget.dart
+++ lib/widgets/category_widget.dart
@@ -17,6 +17,28 @@ class CategoryWidget extends StatefulWidget {
 class _CategoryWidgetState extends State<CategoryWidget> {
   @override
   Widget build(BuildContext context) {
+    const ColorFilter greyscale = ColorFilter.matrix(<double>[
+      0.2126,
+      0.7152,
+      0.0722,
+      0,
+      0,
+      0.2126,
+      0.7152,
+      0.0722,
+      0,
+      0,
+      0.2126,
+      0.7152,
+      0.0722,
+      0,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+    ]);
     return GestureDetector(
       onTap: () {
         ScaffoldMessenger.of(context).hideCurrentSnackBar();
@@ -24,16 +46,13 @@ class _CategoryWidgetState extends State<CategoryWidget> {
           widget.selected = !widget.selected;
         });
       },
-      child: widget.selected == true
+      child: widget.selected
           ? ImageWidget(
               category: widget.category,
               text: widget.text,
             )
           : ColorFiltered(
-              colorFilter: const ColorFilter.mode(
-                Colors.white,
-                BlendMode.saturation,
-              ),
+              colorFilter: greyscale,
               child: ImageWidget(
                 category: widget.category,
                 text: widget.text,
